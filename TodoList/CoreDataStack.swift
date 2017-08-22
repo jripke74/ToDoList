f73d49877d8534f02d571b3ce2cf1fa4da0e7202
Jeff Ripke <jripke74@gmail.com>
2017-08-22 08:15:24 -0700

    created core data model

--- TodoList/CoreDataStack.swift
+++ TodoList/CoreDataStack.swift
@@ -10,33 +10,7 @@ import Foundation
 import CoreData
 
 class CoreDataStack {
-    
-//    private(set) lazy var applicationDocumentDirectory: URL = {
-//        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-//        
-//        let endIndex = urls.index(before: urls.endIndex)
-//        return urls[endIndex]
-//    }()
-//    
-//    private(set) lazy var managedObjectModel: NSManagedObjectModel = {
-//        let modelUrl = Bundle.main.url(forResource: "ToDoList", withExtension: "momd")!
-//        return NSManagedObjectModel(contentsOf: modelUrl)!
-//    }()
-//    
-//    private(set) lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
-//        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-//        let url = self.applicationDocumentDirectory.appendingPathComponent("TodoList.sqlite")
-//        
-//        do {
-//            try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url)
-//        } catch {
-//            print(error)
-//            abort()
-//        }
-//        
-//        return coordinator
-//    }()
-    
+
     lazy var managedObjectContext: NSManagedObjectContext = {
         let container = self.persistentContainer
         return container.viewContext
